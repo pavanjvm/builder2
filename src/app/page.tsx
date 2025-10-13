3923a67380f351666179aea2dda755118930fd56
pavanjvm <pavanprasad0506@gmail.com>
2025-10-13 13:37:57 +0530

    trpc setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
+"use client"
 import {Button} from "@/components/ui/button";
+import {useTRPC} from "@/trpc/client"
+import {useQuery} from "@tanstack/react-query";   
 const page = ()=> {
+  const trpc = useTRPC();
+  
+  const { data } = useQuery(trpc.hello.queryOptions({ text: "Hello!"}));
   return (
     <div>
-      <Button>
-        click me
-      </Button>
+      {data?.greeting}
     </div>
   )
 }
